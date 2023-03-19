0fffe915b27ea70cfd5e3c300f5f624ebebbe143
Olonisakin David <66315270+olonisakindavid@users.noreply.github.com>
2023-03-19 19:38:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+// var user = {
+// 	name: "David",
+// 	age: 34,
+// 	hobby: "Soccer",
+// 	isMarried: false,
+// };
+
+// user.name//access name ....etc
+
+//adding properties to an object
+// user.favouriteFood = "rice";
+// user.isMarried = true;
+
+
+//technically: an array is
+// var user = {
+// 	0: "David",
+// 	1: 34,
+// 	2: "Soccer",
+// 	3: false,
+// };
+
+//Data structures help us organise things better
+//array can be used in like a shopping list, to do list
+//object can be used in llike user information, or a game character properties per user, the powers can be arrays
+//therefore we can have arrays in an object
+
+
+// var user = {
+// 	name: "David",
+// 	age: 34,
+// 	hobby: "Soccer",
+// 	isMarried: false,
+// 	//the list
+// 	powers: ["water", "fire"]
+// };
+//user.hobby ---accessing the object info
+//user.powers[0]; accessing the array
+
+//objects in a list
+// var list = [
+// {
+// 	username: "andy",
+// 	password: "djdj"
+// },
+// {
+// 	username: "jess",
+// 	password: "1123"
+// }
+// ];
+
+// list[0].password//got the object data from the list
+
+
+//adding function to an object
+// var user = {
+// 	name: "David",
+// 	age: 34,
+// 	hobby: "Soccer",
+// 	isMarried: false,
+// 	//the list
+// 	powers: ["water", "fire"],
+// 	shout: function(){
+// 		console.log("HAAAA");
+// 		//other console methods
+// 		console.info("hello")
+// 		console.error("Damn it error")
+// 	}
+// };
+
+// user.shout//function inside an object is a method
+
+
+//empty object
+// user2 = {};
+
+// //empty list
+// list2 = [];
+//even if they are empty, they dont show undefined
+
+// var emptyObj ={}
+
+// //Null Datatype
+// var nullObj = null;
